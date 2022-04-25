b15ef3448af8873411e0a8992c0de173798894e4
chrisw444 <wandersonrodriguesf1@gmail.com>
2022-04-25 22:01:16 +0000

    spes: Switch to wfd component
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>

--- device.mk
+++ device.mk
@@ -381,7 +381,8 @@ TARGET_BOARD_PLATFORM := bengal
 TARGET_COMMON_QTI_COMPONENTS := \
     av \
     gps \
-    vibrator
+    vibrator \
+    wfd
 
 # QMI
 PRODUCT_PACKAGES += \
@@ -487,11 +488,3 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini \
     $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
-
-# WiFi Display
-PRODUCT_PACKAGES += \
-    libnl \
-    libwfdaac_vendor
-
-#PRODUCT_BOOT_JARS += \
-    WfdCommon
