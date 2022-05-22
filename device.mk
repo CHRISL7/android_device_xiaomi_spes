43bbd8fd15746c8d6326aacd7f719da1a134f740
chrisl7 <wandersonrodriguesf1@gmail.com>
2022-05-22 07:51:38 -0500

    test
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- device.mk
+++ device.mk
@@ -413,8 +413,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power@1.2 \
     android.hardware.power@1.2.vendor \
-    android.hardware.power-service-qti \
-    android.hardware.power.stats@1.0-service.mock
+    android.hardware.power-service-qti
 
 # Public libraries
 PRODUCT_COPY_FILES += \
