f9d3d0017b01070d553518f816f21934906e15df
chrisl7 <wandersonrodriguesf1@gmail.com>
2022-07-24 22:49:47 +0530

    spes: Build secure_element@1.2 vendor variant
    [1] - 07-24 11:52:04.036 F/linker  (6791): CANNOT LINK EXECUTABLE "/vendor/bin/hw/qcrild": library "android.hardware.secure_element@1.2.so" not found: needed by /vendor/lib64/libril-qc-hal-qmi.so in namespace (default)
    
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- device.mk
+++ device.mk
@@ -481,7 +481,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.5.vendor \
     android.hardware.radio.config@1.3.vendor \
-    android.hardware.radio.deprecated@1.0.vendor
+    android.hardware.radio.deprecated@1.0.vendor \
+    android.hardware.secure_element@1.2.vendor
 
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.vendor.radio.report_codec=1 \
