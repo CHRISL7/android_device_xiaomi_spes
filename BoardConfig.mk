5744e9451e959cf0bea8741d547f5a75936853cc
chrisl7 <wandersonrodriguesf1@gmail.com>
2022-07-29 05:25:15 +0530

    Revert "spes: Introduce Firmware from V13.0.12.0.RGCMIXM"
    This reverts commit a337566ee11176c6bcbd54ca8c6ec0d2b3f5f8b3.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -186,6 +186,3 @@ CONFIG_ACS := true
 
 # Inherit from the proprietary version
 include vendor/xiaomi/spes/BoardConfigVendor.mk
-
-# Inherit from the firmware
-include vendor/xiaomi/spes-firmware/BoardConfigVendor.mk
