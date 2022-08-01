4056c6324780c47fcb2302cc8b5da86ef03dcb2c
chrisl7 <wandersonrodriguesf1@gmail.com>
2022-08-02 05:14:52 +0530

    Revert "Revert "spes: Move to common Xiaomi fingerprint HIDL""
    This reverts commit 6f72fdcd4dbf51f874678af21fc8687d1e6dfd57.

--- device.mk
+++ device.mk
@@ -323,7 +323,7 @@ LOC_HIDL_VERSION := 4.0
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.spes
+    android.hardware.biometrics.fingerprint@2.3-service.xiaomi
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
@@ -525,7 +525,8 @@ PRODUCT_PACKAGES += \
     init.stnfc.rc \
     init.target.rc \
     ueventd.xiaomi.rc \
-    init.xiaomi.rc
+    init.xiaomi.rc \
+    init.xiaomi.fingerprint.rc
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/init.recovery.qcom.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.qcom.rc \
