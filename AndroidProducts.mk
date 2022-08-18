3b25cf3b1b86939bfa513110891a4690ad8c16ba
chrisl7 <wandersonrodriguesf1@gmail.com>
2022-08-18 14:17:26 +0000

    [DNM-TMP] spes: Arrow bringup
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_spes.mk
+    $(LOCAL_DIR)/arrow_spes.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_spes-user \
-    aosp_spes-userdebug \
-    aosp_spes-eng
+    arrow_spes-user \
+    arrow_spes-userdebug \
+    arrow_spes-eng
